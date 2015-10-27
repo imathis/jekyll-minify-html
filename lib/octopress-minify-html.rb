32a747a228ce1c59927e59ba6bfc1a9dc2ecbb57
Andor Chen <andor.chen.27@gmail.com>
2015-10-27 12:42:24 +0800

    updates hook containers name

--- lib/octopress-minify-html.rb
+++ lib/octopress-minify-html.rb
@@ -32,16 +32,16 @@ module Octopress
       return obj.reduce({}) do |memo, (k, v)|
         memo.tap { |m| m[k.to_sym] = symbolize(v) }
       end if obj.is_a? Hash
-        
-      return obj.reduce([]) do |memo, v| 
+
+      return obj.reduce([]) do |memo, v|
         memo << symbolize(v); memo
       end if obj.is_a? Array
-      
+
       obj
     end
 
     if defined?(Jekyll::Hooks)
-      Jekyll::Hooks.register [:post, :page, :document], :post_render do |item|
+      Jekyll::Hooks.register [:posts, :pages, :documents], :post_render do |item|
         item.output = MinifyHTML.minify(item)
       end
     else
